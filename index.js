c58fbf5f204be21d106002cb13de898872f1255a
Palakpreet Kaur <125293499+Palakx03@users.noreply.github.com>
2023-08-19 20:31:35 +0530

    JavaScript Basics

---
+++ index.js
@@ -0,0 +1,25 @@
+ //comment (why and how )
+//  console.log('Palak'); 
+// let is used for declaring a variable 
+// let name = 'Palakpreet kaur';
+// let age = 21;
+// let person = {
+//    name:'Palakpreet Kaur',
+//    age: 21
+// };
+// person.age = 18;
+// console.log(person.age );   // Dot notation preffered
+
+// person['name'] = 'Mary';// Bracket Notation
+// console.log(person.name );
+
+// let sc = ['red','blue'];                 // Arrays
+// sc[2] = 1;
+// console.log(sc.length);
+
+//Functions
+
+function great(name){
+    console.log('Hello'+" "+ name);
+}
+great('Palak');
